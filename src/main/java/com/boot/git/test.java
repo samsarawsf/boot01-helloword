4bb941aeb9f181825996e0e984bc22d1f25412f0
Wangsuifan <wsf_work@163.com>
2022-05-26 21:53:37 +0800

    hot-fix commit1

--- src/main/java/com/boot/git/test.java
+++ src/main/java/com/boot/git/test.java
@@ -8,5 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
     }
 }
